399e18c6a20de4eeb19ddf02daaecd2a681ec7d6
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-05-14 07:43:56 +0000

    3. Обработал ошибки

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -2,6 +2,7 @@ import { Dispatch } from 'redux'
 import { decksAPI, UpdateDeckParams } from './decks-api.ts'
 import { addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC } from './decks-reducer.ts'
 import { setAppStatus } from '../../app/app-reducer.ts'
+import { isAxiosError } from 'axios'
 
 export const fetchDecksTC = () => async (dispatch: Dispatch) => {
   dispatch(setAppStatus('loading'))
@@ -27,7 +28,23 @@ export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
 }
 
 export const updateDeckTC = (params: UpdateDeckParams) => async (dispatch: Dispatch) => {
-  return decksAPI.updateDeck(params).then((res) => {
+  try {
+    //throw new Error('Boom!')
+    const res = await decksAPI.updateDeck(params)
     dispatch(updateDeckAC(res.data))
-  })
+  } catch (error) {
+    let errorMessage: string
+    
+    if (isAxiosError<ServerError>(error)) {
+      errorMessage = error.response ? error.response.data.errorMessages[0].message : error.message
+    } else {
+      errorMessage = (error as Error).message
+    }
+
+    console.log(errorMessage)
+  }
+}
+
+type ServerError = {
+  errorMessages: Array<{ field: string; message: string }>
 }
